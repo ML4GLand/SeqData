dede03c4e43edd9c8bd4c136d3ab0e305232d289
d-laub <dlaub@ucsd.edu>
2023-04-02 14:34:01 -0700

    Remove functions to write HDF5 SeqData. Expose API from seqdata.

--- seqdata/__init__.py
+++ seqdata/__init__.py
@@ -1,4 +1,4 @@
 """Annotated sequence data"""
 
 from ._core.seqdata import SeqData
-from ._io.read import read_bams, read_bigwigs, read_csvs, read_fasta
+from ._io.read import BAM, VCF, BigWig, FlatFASTA, GenomeFASTA, Table
