855a1c067466b8489889e2e2f80aec6988d646d4
d-laub <dlaub@ucsd.edu>
2023-04-03 17:28:00 -0700

    Add functional creation API.

--- seqdata/__init__.py
+++ seqdata/__init__.py
@@ -1,4 +1,12 @@
 """Annotated sequence data"""
 
 from ._core import SeqData, concat
-from ._io.read import BAM, VCF, BigWig, FlatFASTA, GenomeFASTA, Table
+from ._io.read import (
+    read_bam,
+    read_bigwig,
+    read_flat_fasta,
+    read_genome_fasta,
+    read_table,
+    read_vcf,
+)
+from ._io.readers import BAM, VCF, BigWig, FlatFASTA, GenomeFASTA, Table
