e867542a4732ea1155656e9b30223c6637c87119
d-laub <dlaub@ucsd.edu>
2023-07-04 21:25:45 -0700

    Initial implementation of XArrayDataLoader that supports fast random shuffling.

--- seqdata/torch.py
+++ seqdata/torch.py
@@ -1,3 +1,4 @@
+import warnings
 from itertools import accumulate, chain, repeat
 from typing import (
     Callable,
@@ -221,7 +222,7 @@ def get_torch_dataloader(
     if isinstance(dtypes, torch.dtype):
         dtypes = {k: dtypes for k in variables}
     dim_sizes = [sdata.dims[d] for d in sample_dims]
-    dataset = _cartesian_product([np.arange(d, dtype="uintp") for d in dim_sizes])
+    dataset = _cartesian_product([np.arange(d, dtype=np.uintp) for d in dim_sizes])
 
     def collate_fn(indices: List[NDArray]):
         idx = np.vstack(indices)
@@ -280,3 +281,245 @@ def get_torch_dataloader(
         prefetch_factor=prefetch_factor,  # type: ignore
         persistent_workers=persistent_workers,
     )
+
+
+# TODO: async dataloading. See dask asynchronous operation (https://distributed.dask.org/en/stable/asynchronous.html).
+# ! TODO: fix issue when variables only have a subset of sample_dims
+# ! TODO: allow in-memory sdata
+# TODO: add parameters for `sampler`, `pin_memory`, `drop_last`
+class XArrayDataLoader:
+    def __init__(
+        self,
+        sdata: xr.Dataset,
+        sample_dims: Union[str, List[str]],
+        variables: Union[str, List[str]],
+        transform: Optional[Callable[[Dict[str, NDArray]], Dict[str, NDArray]]] = None,
+        dtypes: Union[torch.dtype, Dict[str, torch.dtype]] = torch.float32,
+        batch_size: int = 1,
+        prefetch_factor: int = 2,
+        shuffle: bool = False,
+        seed: Optional[int] = None,
+        return_tuples: bool = False,
+    ) -> None:
+        """Get an XArray DataLoader that supports substantially faster out-of-core
+        dataloading from chunked storage formats than a PyTorch DataLoader. Note the
+        absence of concurrency parameters. This is intentional: concurrent I/O is
+        enabled by instantiating a `dask.distributed.Client`.
+
+        Parameters
+        ----------
+        sdata : xr.Dataset
+        sample_dims : Union[str, List[str]]
+            Dimensions to sample over (i.e. what dimensions would you index over to get
+            a single instance?)
+        variables : Union[str, List[str]]
+            What variables to load data from.
+        transform : Optional[Callable[[Dict[str, NDArray]], Dict[str, NDArray]]], optional
+            A function to transform batches after loading them into memory. Should take
+            a dictionary of numpy arrays, each corresponding to a `variable`, transform
+            them, and return the result as a dictionary with the same keys. By default
+            no transforms will be applied.
+        dtypes : Union[torch.dtype, Dict[str, torch.dtype]], optional
+            What dtype to convert each batch array to. Either a single dtype to convert
+            all variables or a dictionary mapping variables to dtypes. By default
+            `torch.float32`.
+        batch_size : int, optional
+            How many instances per batch, by default 1
+        prefetch_factor : int, optional
+            What multiple of chunks to prefetch, by default 2. Tune this and the Zarr
+            chunk sizes appropriately to control peak memory usage and balance speed and
+            memory usage. A higher prefetch factor improves speed but uses more memory.
+        shuffle : bool, optional
+            Whether to randomly shuffle the dataset on each epoch, by default False
+        seed : Optional[int], optional
+            Seed for random shuffling, by default None
+        return_tuples : bool, optional
+            Whether to yield tuples (or dictionaries). By default False.
+
+        Raises
+        ------
+        ValueError
+            When `variables` specifies variables that aren't in the Dataset.
+        ValueError
+            When variables have different chunk sizes in any of the sample dimensions.
+
+        Notes
+        -----
+        **Data flow**
+
+        1. Load contiguous chunks of data from the dataset into buffers that are larger
+        than the batch size.
+        2. Yield batches from the buffer until the buffer is empty, then repeat 1.
+
+        **Random shuffling**
+
+        We implement random shuffling by prefetching random chunks and then randomly
+        sampling data from within those chunks. It is possible (although unlikely) that
+        the data may have structure that isn't randomized due to the lack of fully
+        random sampling.
+        """
+        if isinstance(sample_dims, str):
+            sample_dims = [sample_dims]
+        if isinstance(variables, str):
+            variables = [variables]
+
+        variables_not_in_ds = set(variables) - set(sdata.data_vars.keys())
+        if variables_not_in_ds:
+            raise ValueError(
+                f"Got variables that are not in the dataset: {variables_not_in_ds}"
+            )
+
+        if isinstance(dtypes, torch.dtype):
+            self.dtypes = {k: dtypes for k in variables}
+        else:
+            self.dtypes = dtypes
+
+        self.sdata = sdata
+        self.variables = variables
+        variable = sdata[variables[0]]
+        self.first_variable = variable
+
+        chunksizes = {}
+        for dim in sample_dims:
+            dim_chunk_sizes = set()
+            for v in sdata[variables].data_vars.values():
+                if dim in v.dims:
+                    dim_chunk_sizes.add(v.data.chunksize[v.get_axis_num(dim)])
+            chunksizes[dim] = dim_chunk_sizes
+        discrepant_chunk_sizes = {k: v for k, v in chunksizes.items() if len(v) > 1}
+        if len(discrepant_chunk_sizes) > 1:
+            raise ValueError(
+                f"""Variables have different chunksizes in the sample dimensions.\n
+                Dimensions with discrepant chunksizes: {list(discrepant_chunk_sizes.keys())}.\n
+                Rechunk the variables in the sample dimensions so they are the same.
+                """
+            )
+        self.chunksizes: Dict[str, int] = {k: v.pop() for k, v in chunksizes.items()}
+
+        self.sample_dims = sample_dims
+        self.instances_per_chunk = np.product(list(self.chunksizes.values()))
+        chunks_per_batch = -(-batch_size // self.instances_per_chunk)
+        self.n_prefetch_chunks = prefetch_factor * chunks_per_batch
+        sample_dim_shape = [
+            variable.shape[variable.get_axis_num(d)] for d in sample_dims  # type: ignore
+        ]
+        self.n_instances = np.product(sample_dim_shape)
+        if batch_size > self.n_instances:
+            warnings.warn(
+                f"""Batch size {batch_size} is larger than the number of instances in 
+                the dataset {self.n_instances}. Reducing batch size to maximum number of
+                instances."""
+            )
+            self.batch_size = self.n_instances
+        else:
+            self.batch_size = batch_size
+        self.max_batches = -(-self.n_instances // self.batch_size)
+        self.buffer_size = self.instances_per_chunk * self.n_prefetch_chunks
+
+        self.rng = np.random.default_rng(seed)
+        self.shuffle = shuffle
+        self.transform = transform
+        self.return_tuples = return_tuples
+
+        chunk_start_idx: Dict[str, NDArray[np.int64]] = {}
+        for dim in self.sample_dims:
+            chunk_start_idx[dim] = np.zeros(
+                len(self.first_variable.chunksizes[dim]), dtype=np.int64
+            )
+            np.cumsum(
+                self.first_variable.chunksizes[dim][:-1], out=chunk_start_idx[dim][1:]
+            )
+        self.chunk_idxs = _cartesian_product(list(chunk_start_idx.values()))
+
+    def __len__(self):
+        return self.max_batches
+
+    def __iter__(self):
+        # which slice of chunks is going into the buffer
+        self.chunk_slice = slice(0, self.n_prefetch_chunks)
+        # which slice of the buffer is going into the batch
+        self.buffer_slice = slice(0, self.batch_size)
+        # which slice of the batch is getting pulled & processed
+        # i.e. batch[self.batch_slice] = self.buffer[self.buffer_slice]
+        self.batch_slice = slice(0, self.batch_size)
+        self.current_batch = 0
+        if self.shuffle:
+            self.chunk_idxs = self.rng.permutation(self.chunk_idxs, axis=0)
+        self._flush_and_fill_buffers()
+        return self
+
+    def _flush_and_fill_buffers(self):
+        # Each buffer in buffers will have shape (self.buffer_size, ...)
+        self.buffers: Dict[str, NDArray] = {}
+        # (n_chunks, n_dim)
+        chunk_idx = self.chunk_idxs[self.chunk_slice]
+        self.chunk_slice = slice(
+            self.chunk_slice.start, self.chunk_slice.start + self.n_prefetch_chunks
+        )
+        for var in self.variables:
+            buffer = []
+            for chunk in chunk_idx:
+                selector = {
+                    d: slice(start, start + self.chunksizes[d])
+                    for start, d in zip(chunk, self.sample_dims)
+                }
+                buffer.append(
+                    self.sdata[var].isel(selector, missing_dims="ignore").to_numpy()
+                )
+            buffer = np.concatenate(buffer)
+            self.buffers[var] = self.rng.permutation(buffer) if self.shuffle else buffer
+
+    def _apply_dtypes(self, batch: Dict[str, NDArray]):
+        out = {
+            k: torch.as_tensor(v, dtype=dtype)
+            for (k, dtype), v in zip(self.dtypes.items(), batch.values())
+        }
+        return out
+
+    def __next__(self):
+        if self.current_batch == self.max_batches:
+            raise StopIteration
+
+        # init empty batch arrays
+        batch: Dict[str, NDArray] = {
+            k: np.empty_like(v.data, shape=(self.batch_size, *v.shape[1:]))
+            for k, v in self.buffers.items()
+        }
+
+        overshoot = self.buffer_slice.stop - len(self.buffers[self.variables[0]])
+
+        # buffers don't have enough data to fill the batch
+        if overshoot > 0:
+            # grab what they do have
+            self.buffer_slice = slice(self.buffer_slice.start, self.buffer_size)
+            self.batch_slice = slice(0, self.batch_size - overshoot)
+            for var, buffer in self.buffers.items():
+                batch[var][self.batch_slice] = buffer[self.buffer_slice]
+
+            # fetch more data
+            self._flush_and_fill_buffers()
+
+            # setup to fill the rest of the batch
+            self.buffer_slice = slice(0, overshoot)
+            self.batch_slice = slice(self.batch_slice.stop, self.batch_size)
+
+        for var, buffer in self.buffers.items():
+            batch[var][self.batch_slice] = buffer[self.buffer_slice]
+
+        # setup for next batch
+        self.buffer_slice = slice(
+            self.buffer_slice.stop, self.buffer_slice.stop + self.batch_size
+        )
+        self.batch_slice = slice(0, self.batch_size)
+        self.current_batch += 1
+
+        # apply transforms, if any
+        if self.transform is not None:
+            batch = self.transform(batch)
+
+        out = self._apply_dtypes(batch)
+
+        if self.return_tuples:
+            return tuple(out.values())
+
+        return out
